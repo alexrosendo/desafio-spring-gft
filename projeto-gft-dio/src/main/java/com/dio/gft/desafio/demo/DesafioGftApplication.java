4b2293eae111ab824cfa91274585f4a03f05798f
Alex Rosendo <alexrosendo23@gmail.com>
2022-05-28 20:37:46 -0300

    update

---
+++ projeto-gft-dio/src/main/java/com/dio/gft/desafio/demo/DesafioGftApplication.java
@@ -0,0 +1,13 @@
+package com.dio.gft.desafio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGftApplication.class, args);
+	}
+
+}
